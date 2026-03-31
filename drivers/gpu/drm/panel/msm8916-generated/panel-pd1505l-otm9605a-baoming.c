600bc6c81121f21ddd208652634d79850c0e218b
QingXian <126559216+wufengwuyong@users.noreply.github.com>
2026-03-31 20:57:07 +0800

    Fix formatting issues in panel-pd1505l-otm9605a-baoming.c

--- drivers/gpu/drm/panel/msm8916-generated/panel-pd1505l-otm9605a-baoming.c
+++ drivers/gpu/drm/panel/msm8916-generated/panel-pd1505l-otm9605a-baoming.c
@@ -275,7 +275,7 @@ static int pd1505l_otm9605a_540p_baoming_prepare(struct drm_panel *panel)
 	int ret;
 
 	ret = regulator_bulk_enable(ARRAY_SIZE(pd1505l_otm9605a_540p_baoming_supplies),
-				    ctx->supplies);
+  		      ctx->supplies);
 	if (ret < 0) {
 		dev_err(dev, "Failed to enable regulators: %d\n", ret);
 		return ret;
@@ -288,7 +288,7 @@ static int pd1505l_otm9605a_540p_baoming_prepare(struct drm_panel *panel)
 		dev_err(dev, "Failed to initialize panel: %d\n", ret);
 		gpiod_set_value_cansleep(ctx->reset_gpio, 1);
 		regulator_bulk_disable(ARRAY_SIZE(pd1505l_otm9605a_540p_baoming_supplies),
-			       ctx->supplies);
+  		      	ctx->supplies);
 		return ret;
 	}
 
